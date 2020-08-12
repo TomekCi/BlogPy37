e486ab153b97056acaa6d3822694d0c5e1fc5db8
TomekCi <tomekciesz@gmail.com>
2020-08-12 23:34:33 +0200

    some cleaning

--- app.py
+++ app.py
@@ -28,9 +28,9 @@ login_manager.init_app(app)
 
 
 class RegisterForm(Form):
-    username = StringField('username', [validators.Length(min=5, max=30)])
-    email = StringField('emailaddress', [validators.Length(min=5, max=30)])
-    password = PasswordField('passwordhash', [
+    username = StringField('User name', [validators.Length(min=5, max=30)])
+    email = StringField('E-mail address', [validators.Length(min=5, max=30)])
+    password = PasswordField('Password', [
         validators.Length(min=5, max=15),
         validators.EqualTo('confirm', message='Passwords do not mach')
     ])
@@ -38,8 +38,8 @@ class RegisterForm(Form):
 
 
 class LoginForm(Form):
-    username = StringField('username', [validators.Length(min=5, max=30)])
-    password = PasswordField('passwordHash', [validators.Length(min=5, max=15)])
+    username = StringField('User name', [validators.Length(min=5, max=30)])
+    password = PasswordField('Password', [validators.Length(min=5, max=15)])
 
 
 @login_manager.user_loader
@@ -86,72 +86,9 @@ def register():
     return render_template('register.html', form=form)
 
 
-@app.route("/articles")
-def articles():
-    return render_template('articles.html', articles = Articles)
-
-
-@app.route("/article/<int:id>")
-def article(id):
-    return render_template('article.html', id = id, article = article)
-
-
 @app.route("/")
 def main():
     return render_template('index.html')
-
-
-@app.route('/posts', methods=['GET', 'POST'])
-def posts():
-    if request.method == 'POST':
-        post_title = request.form['title']
-        post_content = request.form['content']
-        post_author = request.form['author']
-        new_post = Post(title=post_title, content=post_content, author=post_author)
-        db.session.add(new_post)
-        db.session.commit()
-        return redirect('/posts')
-    elif request.method == 'GET':
-        all_posts = Post.query.order_by(Post.date_posted).all()
-        return render_template('posts.html', posts=all_posts)
-
-
-@app.route('/posts/delete/<int:id>')
-def delete(id):
-    post = Post.query.get_or_404(id)
-    db.session.delete(post)
-    db.session.commit()
-    return redirect('/posts')
-
-
-@app.route('/posts/edit/<int:id>', methods=['GET', 'POST'])
-def edit(id):
-
-    post = Post.query.get_or_404(id)
-
-    if request.method == 'POST':
-        post.title = request.form['title']
-        post.content = request.form['content']
-        post.author = request.form['author']
-        db.session.commit()
-        return redirect('/posts')
-    else:
-        return render_template('edit.html', post=post)
-
-
-@app.route('/posts/new', methods=['GET', 'POST'])
-def new_posts():
-    if request.method == 'POST':
-        post_title = request.form['title']
-        post_content = request.form['content']
-        post_author = request.form['author']
-        new_post = Post(title = post_title, content = post_content)
-        db.session.add(new_post)
-        db.session.commit()
-        return redirect('/posts')
-    elif request.method == 'GET':
-        all_posts = Post.query.order_by(Post.date_posted).all()
-        return render_template('new_post.html', posts=all_posts)
 
 
 if __name__ == '__main__':
